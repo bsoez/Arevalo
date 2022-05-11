258deec431abc859776b51bbdf96dcb29d5c0a09
María Belén Arévalo Esquivel <belensoez@gmail.com>
2022-05-11 14:37:57 -0500

    JSON

--- Parcial3/BotonJSON/getRegistro.php
+++ Parcial3/BotonJSON/getRegistro.php
@@ -6,7 +6,7 @@ $registro ['Categoria'] = 'Mundo abierto';
 $registro ['anioJuego'] = '2018';
 $registro ['nomComp'] = 'Rockstar games';
 $registro ['precioJuego'] = '$ 60';
-$registro ['Descripcion'] = 'Juego de mundo abierto de vaqueros y armas.';
+$registro ['Descripcion'] = 'Juego de mundo abierto, vaqueros, caballos y armas.';
 $registro ['ultimaVersion'] = '5.1';
 
 $registroJson = json_encode($registro);
