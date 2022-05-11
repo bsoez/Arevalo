141fb6661bb3316457e812ae4c4e5d6f1431f8b9
María Belén Arévalo Esquivel <belensoez@gmail.com>
2022-05-11 14:34:55 -0500

    JSON

---
+++ Parcial3/BotonJSON/getRegistro.php
@@ -0,0 +1,14 @@
+<?php
+
+$registro ['idjuego'] = '20125';
+$registro ['nomJuego'] = 'Red dead redemtion 2';
+$registro ['Categoria'] = 'Mundo abierto';
+$registro ['anioJuego'] = '2018';
+$registro ['nomComp'] = 'Rockstar games';
+$registro ['precioJuego'] = '$ 60';
+$registro ['Descripcion'] = 'Juego de mundo abierto de vaqueros y armas.';
+$registro ['ultimaVersion'] = '5.1';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
