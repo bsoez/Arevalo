5ff1a79895dcf3c7692b35f9f93105b968789df2
María Belén Arévalo Esquivel <belensoez@gmail.com>
2022-05-22 23:22:42 -0500

    Act

--- Parcial3/PracticaFetch/PracticaPromesas.js
+++ Parcial3/PracticaFetch/PracticaPromesas.js
@@ -23,9 +23,3 @@ window.onload = function() {
     } 
 }
 
-document.getElementById("btnFetch").addEventListener("click",function(){
-            
-    let promise = fetch('peticion.php');
-
-    promise.then(respuesta => respuesta.json()).then(datos => console.log(datos));
-});
