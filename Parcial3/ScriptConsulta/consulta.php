85a70cf71831895509a676f2e5db20b2f7ecb486
María Belén Arévalo Esquivel <belensoez@gmail.com>
2022-05-23 22:56:58 -0500

    act

---
+++ Parcial3/ScriptConsulta/consulta.php
@@ -0,0 +1,16 @@
+<?php
+include "conectar.php";
+
+
+try{
+    $con= new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
+} catch(PDOException $e) {
+    echo "Error de conexion con la base de datos";
+    echo $e->getMessage();
+    exit();
+
+}
+
+$con->setAtribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ Parcial3/ScriptConsulta/getRegistro.php
@@ -0,0 +1,29 @@
+<?php
+
+$id=2;
+$hostname = 'localhost';
+$database = 'n19100144';
+$username = 'root';
+$passwword = 'Default123';
+try{
+    $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$passwword);
+} catch(PDOException $e){
+    echo $e->getMessage();
+    exit();
+}
+//$con->setAttribute(PDO::ATT_ERRMODE,PDO::ERRMODE_EXCEPTION);
+try{
+   
+    $consultaSql = "Select idID,NombreCom,NombreDelJuego,Rango,Edad,Pais from Jugadores where idID=" .$id;
+    $consulta = $con -> prepare($consultaSql);
+    $consulta -> execute();
+    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    $consulta->closeCursor();
+}catch(PDOException $e){
+    echo "Error d la consulta";
+    echo $e->getMessage();
+}
+$resultadoJSON = json_encode($resultado);
+echo $resultadoJSON;
+
+?>
